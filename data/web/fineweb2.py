81724bbc3ca7a3246f3a38e7a14b9c7ffb149a12
Oligou <oliviergouvert@gmail.com>
2025-03-18 11:48:03 +0100

    Refactor, and add a local mode

--- data/web/fineweb2.py
+++ data/web/fineweb2.py
@@ -1,39 +1,35 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from utils import create_pipeline, create_parser, MAIN_PATH
+
 from datatrove.pipeline.readers import ParquetReader
 from datatrove.pipeline.writers import JsonlWriter
-from datatrove.executor.jeanzay import JZSlurmPipelineExecutor
-import argparse
-
-parser = argparse.ArgumentParser("Load Fineweb 2 dataset")
-parser.add_argument(
-    "--language", type=str, default="fra_Latn", help=""
-)
-parser.add_argument("--debug", action="store_true", help="Debug mode")
 
 if __name__ == "__main__":
+    parser = create_parser()
+    parser.add_argument(
+        "--language", type=str, default="fra_Latn", help="Language to process"
+    )
     args = parser.parse_args()
     
-    OUTPUT_PATH = "/lustre/fsn1/projects/rech/qgz/commun/datasets/training"
-    if args.debug:
-        OUTPUT_PATH += '/debug'
+    dataset_name="fineweb2"
     language = args.language
+    output_path = os.path.join(MAIN_PATH, dataset_name)
 
-    main_processing_executor = JZSlurmPipelineExecutor(
-        job_name=f"fineweb2",
-        pipeline=[ 
-            ParquetReader(
-                f"hf://datasets/HuggingFaceFW/fineweb-2/data/{language}/train", 
-                limit=1000 if args.debug else -1
-                ),
-            JsonlWriter(f"{OUTPUT_PATH}/fineweb-2/data/{language}/train")
-        ],
-        sbatch_args={"account": "qgz@cpu"},
-        tasks=1 if args.debug else 50, 
-        cpus_per_task=10,
-        time="02:00:00",
-        logging_dir=f"{OUTPUT_PATH}/fineweb-2/logs/{language}/train",
-        qos="qos_cpu-t3",
-        partition="prepost",
-        condaenv="datatrove-env",
+    pipeline=[ 
+        ParquetReader(
+            f"hf://datasets/HuggingFaceFW/fineweb-2/data/{language}/train", 
+            ),
+        JsonlWriter(f"{output_path}/data/{language}/train")
+    ]
+
+    main_processing_executor = create_pipeline(
+        pipeline, dataset_name,
+        output_path=output_path,
+        debug=args.debug,
+        local=args.local,
     )
 
     main_processing_executor.run()
