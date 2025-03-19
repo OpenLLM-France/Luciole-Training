d2a6ee4d5e849d5b1e0e4abf7d646d28075196d9
Oligou <oliviergouvert@gmail.com>
2025-03-19 11:26:30 +0100

    Add scripts

---
+++ data/scripts/merge_stats.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+data_path=$DATA/fineweb2/logs/fra_Latn/clusters/stats
+
+python ~/datatrove/src/datatrove/tools/merge_stats.py $data_path 
--- data/set_env.sh
+++ data/set_env.sh
@@ -5,3 +5,4 @@ module load anaconda-py3/2023.09
 conda activate datatrove-env
 
 export HF_HOME=$ALL_CCFRSCRATCH/.cache/huggingface
+export DATA=$ALL_CCFRSCRATCH/datasets/training
