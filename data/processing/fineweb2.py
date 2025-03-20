8318bf97b5ed4e957713ef7ca1cee84401a1c827
Oligou <oliviergouvert@gmail.com>
2025-03-20 15:19:14 +0100

    Decompose fineweb2 pipeline. Add regex for copyrights

--- data/processing/fineweb2.py
+++ data/processing/fineweb2.py
@@ -11,6 +11,7 @@ from datatrove.data import DocumentsPipeline
 from datatrove.pipeline.writers.disk_base import DiskWriter
 from datatrove.data import Document
 from datatrove.pipeline.filters.base_filter import BaseFilter
+from datatrove.pipeline.filters import RegexFilter
 
 class FinewebDocumentCleaning(BaseFilter):
 
@@ -19,30 +20,27 @@ class FinewebDocumentCleaning(BaseFilter):
     def __init__(
         self,
         exclusion_writer: DiskWriter = None,
-        log_repeated_lines: bool = False,
     ):
         super().__init__(exclusion_writer)
-        self.log_repeated_lines = log_repeated_lines
 
     def filter(self, doc: Document) -> bool | tuple[bool, str]:
+        ## Line removal
         lines = doc.text.strip().splitlines()
-        if self.log_repeated_lines:
-            doc.metadata['repeated_line'] = False
-        # Iterating on lines
-        unique_lines = []
-        for line in lines:
-            if "|" in line: # | is a separator in markdown tables
-                unique_lines.append(line)
-            elif not unique_lines or line != unique_lines[-1]:
-                unique_lines.append(line)
-            else:
-                self.stat_update("repeated_line")
-                if self.log_repeated_lines:
-                    doc.metadata['repeated_line'] = True
-        doc.text = '\n'.join(unique_lines)
-        # Check if the document is empty after cleaning
-        if not doc.text.strip():
-            return False
+        kept_lines = []
+        for i, line in enumerate(lines, 0):
+            if len(lines) > 1:
+                if i == 0 and (line in lines[1]):
+                    self.stat_update("first-line-included-in-second")
+                    continue
+                if i == 1 and (line in lines[0]):
+                    self.stat_update("second-line-included-in-first")
+                    continue
+            if kept_lines and (line != '') and ('|' not in line) and (line == kept_lines[-1]):
+                self.stat_update("consecutive-repeated-lines")
+                continue
+            kept_lines.append(line)
+            self.stat_update("line-kept")
+        doc.text = '\n'.join(kept_lines)
         return True
 
 class Rehydrater(PipelineStep):
@@ -72,25 +70,62 @@ if __name__ == "__main__":
     
     dataset_name="fineweb2"
     language = args.language
-
-    # Collect data
     output_path = os.path.join(MAIN_PATH, dataset_name)
+
+    ## Collect data
     pipeline=[ 
         ParquetReader(
             f"hf://datasets/HuggingFaceFW/fineweb-2/data/{language}/train", 
             ),
         FinewebDocumentCleaning(),
+        JsonlWriter(
+            f"{output_path}/data/{language}/train",        
+            )
+    ]
+    main_processing_executor = create_pipeline(
+        pipeline, dataset_name,
+        debug=args.debug,
+        local=args.local,
+        logging_dir=f"{output_path}/logs/{language}/train",
+    )
+
+    ## Split by clusters
+    pipeline=[ 
+        JsonlReader(
+            f"{output_path}/data/{language}/train"
+        ),
         Rehydrater(),
         JsonlWriter(
             f"{output_path}/data/{language}/clusters",
             output_filename="${cluster_size_group}/${rank}.jsonl.gz", 
         )
     ]
-    main_processing_executor = create_pipeline(
+    split_executor = create_pipeline(
         pipeline, dataset_name,
         debug=args.debug,
         local=args.local,
         logging_dir=f"{output_path}/logs/{language}/clusters",
+        depends_on=main_processing_executor,
     )
-    main_processing_executor.run()
+    split_executor.run()
 
+    ## Extract potential copyrights - there are not remove from the data!
+    pipeline=[ 
+        JsonlReader(
+            f"{output_path}/data/{language}/train"
+        ),
+        RegexFilter(
+            regex_exp=r"(Copyright|copyright|©|All\s+rights\s+reserved)",
+            exclusion_writer=JsonlWriter(
+                f"{output_path}/data/{language}/potential_copyrights" 
+                )
+        )
+    ]
+    copyright_executor = create_pipeline(
+        pipeline, dataset_name,
+        debug=args.debug,
+        local=args.local,
+        logging_dir=f"{output_path}/logs/{language}/clusters",
+        depends_on=main_processing_executor,
+    )
+    copyright_executor.run()
