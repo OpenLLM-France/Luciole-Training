baefe1ab0d5aee4592ed0b758fed87cf1163824b
Audran Bert <bert.audran@gmail.com>
2025-03-24 14:33:48 +0100

    add data arg

--- ablations/set_env.sh
+++ ablations/set_env.sh
@@ -5,7 +5,7 @@ export TRANSFORMERS_OFFLINE=1
 export HF_HUB_OFFLINE=1
 
 export OpenLLM_OUTPUT=$ALL_CCFRSCRATCH/OpenLLM-BPI-output
-export OpenLLM_DATA=$ALL_CCFRSCRATCH/preprocessed_data/Lucie/lucie_tokens_65k_grouped/Wikipedia--fr_text_document
+export OpenLLM_DATA=$ALL_CCFRSCRATCH/preprocessed_data/Lucie/lucie_tokens_65k_grouped
 
 module purge
 module load arch/h100 nemo/2.1.0
