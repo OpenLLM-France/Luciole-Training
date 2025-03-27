b675ed44be7e692fb0ceb5d95fd6d81671b825c7
Oligou <oliviergouvert@gmail.com>
2025-03-27 14:17:10 +0100

    Update stats

--- data/scripts/fineweb2_stats.py
+++ data/scripts/fineweb2_stats.py
@@ -1,49 +1,70 @@
+import os
 import json
 import re
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
+from matplotlib import colormaps
 
-path = f"{os.getenv('DATA')}/fineweb2/logs/fra_Latn/clusters"
-os.makedirs(f"{path}/fig", exist_ok=True)
+MAIN_PATH = os.getenv('OpenLLM_OUTPUT')
+os.makedirs(f"{MAIN_PATH}/fig", exist_ok=True)
+os.makedirs(f"{MAIN_PATH}/stats", exist_ok=True)
 
-with open(f"{path}/stats/merged_stats.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
+def read_data(data_path):
+    with open(f"{data_path}/stats/merged_stats.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
 
-out = []
-for k, v in data[-1]['stats'].items():
-    if 'cluster' in k:
-        out.append({
-            'cluster_size': re.search(r"cluster_size:(.*)/", k).group(1),
-            'documents': v['total']
-        })
+    out = []
+    for k, v in data[-1]['stats'].items():
+        if 'cluster' in k:
+            out.append({
+                'cluster_size': re.search(r"cluster_size:(.*)/", k).group(1),
+                'documents': v['total']
+            })
+    return pd.DataFrame(out)
 
-order = ["1", "2", "3", "4", "5-100", "100-1000", "1000+"]
-weights = [1, 2, 3, 3, 5, 8, 1]
+def order(df):
+    order = ["1", "2", "3", "4", "5-100", "100-1000", "1000+"]
+    df['cluster_size'] = pd.Categorical(df['cluster_size'], categories=order, ordered=True)
+    df = df.sort_values('cluster_size')
+    return df
 
-df = pd.DataFrame(out)
-df['cluster_size'] = pd.Categorical(df['cluster_size'], categories=order, ordered=True)
-df = df.sort_values('cluster_size')
-df['weights'] = weights
+if __name__ == "__main__":
+    languages = ['fra_Latn', 'spa_Latn', 'ita_Latn', 'deu_Latn']
 
-total_docs = df['documents'].sum()
-total_docs_weighted = (df['documents'] * df['weights']).sum()
-print(f"Number of documents: {total_docs:,}")
-print(f'Number of documents after rehydratation: {total_docs_weighted:,}')
-print(f'Ratio: x {total_docs_weighted / total_docs:.2f}')
+    colormap = colormaps['tab20']  # Using tab20 colormap
+    mapping = dict(zip(
+        ["1", "2", "3", "4", "5-100", "100-1000", "1000+"],
+        [1, 2, 3, 3, 5, 8, 1]
+    ))
+ 
+    all_data = []
+    for language in languages:
+        data_path = f"{MAIN_PATH}/datasets/fineweb2/logs/{language}/clusters"
+        df = read_data(data_path)
+        df['language'] = language
+        all_data.append(df)
 
-# Plotting 
-plt.figure(figsize=(10, 5))
-plt.bar(df['cluster_size'], df['documents'])
-plt.yscale("log")  # Set y-axis to logarithmic scale
-plt.xlabel("Cluster Size")
-plt.ylabel("Number of Documents")
-plt.title("Number of Documents per Cluster Size")
-plt.savefig(f"{path}/fig/cluster_size_documents.png", dpi=300)
+    df = pd.concat(all_data)
+    df = order(df)
+    df['weight'] = df['cluster_size'].map(mapping)
+    df['upsampled_doc'] = df['weight'] * df['documents']
+    df.to_csv(f'{MAIN_PATH}/stats/fineweb2.csv')
 
-plt.figure(figsize=(10, 5))
-plt.bar(df['cluster_size'], df['weights'])
-plt.xlabel("Cluster Size")
-plt.ylabel("Upsampling Weight")
-plt.title("Upsampling Weight per Cluster Size")
-plt.savefig(f"{path}/fig/upsampling_weight_documents.png", dpi=300)
+    # Plot 1: Total number of documents by language
+    plt.figure(figsize=(12, 6))
+    df_grouped = df.groupby('language').agg({'documents': 'sum', 'upsampled_doc': 'sum'})
+    ax = df_grouped.plot.bar(rot=0)
+    plt.yscale("log")
+
+    # Add values on top of each bar in scientific notation with 2 decimal places
+    for container in ax.containers:
+        ax.bar_label(container, labels=[f'{v.get_height():.2e}' for v in container], label_type='edge', fontsize=10, color='black')
+    plt.savefig(f"{MAIN_PATH}/fig/fineweb2_total_num_documents.png")
+
+    # Plot 2: Number of documents by cluster size and language
+    plt.figure(figsize=(12, 6))
+    df_pivot = df.pivot(index='cluster_size', columns='language', values='documents')
+    ax = df_pivot.plot.bar(rot=0)
+    plt.yscale("log")
+    plt.savefig(f"{MAIN_PATH}/fig/fineweb2_num_documents_by_cluster.png")
--- data/scripts/merge_stats.sh
+++ data/scripts/merge_stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find "$DATA" -type d -name "stats" | while read data_path; do
+find "${OpenLLM_OUTPUT}/datasets" -type d -name "stats" | while read data_path; do
     output=$data_path/merged_stats.json
     if [ ! -f "$output" ]; then
         echo -e "\n---------------\nProcessing $data_path\n---------------"
