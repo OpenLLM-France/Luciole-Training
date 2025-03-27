8be8931a8442360ee8d5c9d36a3fd51708b90b82
Oligou <oliviergouvert@gmail.com>
2025-03-27 14:17:10 +0100

    Update stats

--- data/scripts/print_stats.sh
+++ data/scripts/print_stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find "$DATA" -type d -name "stats" | while read data_path; do
+find "${OpenLLM_OUTPUT}/datasets" -type d -name "stats" | while read data_path; do
     relative_path="${data_path#"$DATA"/}"  # Remove $DATA prefix
-    echo -e "\n---------------\nPath \$DATA/$relative_path\n---------------"
+    echo -e "\n---------------\nPath \$OpenLLM_OUTPUT/datasets/$relative_path\n---------------"
     python print_stats.py --stats_dir "$data_path" 
 done
