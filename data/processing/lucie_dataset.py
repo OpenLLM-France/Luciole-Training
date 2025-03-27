1bef5815f654933c52348940d898ee3ec3381db1
Oligou <oliviergouvert@gmail.com>
2025-03-27 15:55:55 +0100

    Add lucie dataset

---
+++ data/processing/lucie_dataset.py
@@ -0,0 +1,40 @@
+from utils import create_pipeline, create_parser, get_data_path
+
+from datatrove.pipeline.readers import HuggingFaceDatasetReader
+from datatrove.pipeline.writers import JsonlWriter
+
+from datasets import load_dataset_builder
+
+config_names = list(load_dataset_builder("OpenLLM-France/Lucie-Training-Dataset").builder_configs)
+
+if __name__ == "__main__":
+    parser = create_parser()
+    parser.add_argument(
+        "--name", type=str, default="default", help="Subset to load", choices=config_names
+    )
+    parser.add_argument(
+        "--revision", type=str, default="main", help="Revision", choices=["main", "v1.2"]
+    )
+    args = parser.parse_args()
+
+    name = args.name
+    revision = args.revision
+    MAIN_PATH = get_data_path(args.debug, args.local)
+    
+    pipeline=[ 
+        HuggingFaceDatasetReader(
+            "OpenLLM-France/Lucie-Training-Dataset",
+            {"name": name, "revision": revision, "split": "train"},
+            streaming=True
+            ),
+        JsonlWriter(f"{MAIN_PATH}/lucie_dataset/{name}/output")
+    ]
+
+    main_processing_executor = create_pipeline(
+        pipeline, name,
+        debug=args.debug,
+        local=args.local,
+        logging_dir=f"{MAIN_PATH}/lucie_dataset/{name}/logs",
+    )
+
+    main_processing_executor.run()
