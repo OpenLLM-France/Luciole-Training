3810da230429c4e1f35b17717041eb7764beb83c
Oligou <oliviergouvert@gmail.com>
2025-03-28 09:34:01 +0100

    Add pes2o

---
+++ data/processing/pes2o.py
@@ -0,0 +1,32 @@
+from utils import create_pipeline, create_parser, get_data_path
+
+from datatrove.pipeline.readers import HuggingFaceDatasetReader
+from datatrove.pipeline.writers import JsonlWriter
+
+if __name__ == "__main__":
+    parser = create_parser()
+    parser.add_argument(
+        "--olmo", action="store_true", help="Take dataset from Olmo2"
+    )
+    args = parser.parse_args()
+    MAIN_PATH = get_data_path(args.debug, args.local)
+
+    dataset_name = "pes2o" 
+
+    pipeline=[ 
+        HuggingFaceDatasetReader(
+            "allenai/olmo-mix-1124", 
+            {"name": "pes2o", "split": "train"},
+            streaming = True,
+            ),
+        JsonlWriter(f"{MAIN_PATH}/{dataset_name}/output")
+    ]
+
+    main_processing_executor = create_pipeline(
+        pipeline, dataset_name,
+        debug=args.debug,
+        local=args.local,
+        logging_dir=f"{MAIN_PATH}/{dataset_name}/logs",
+    )
+
+    main_processing_executor.run()
