4e96460376ec161ef0e28a22cf163240731a4fc9
Audran Bert <bert.audran@gmail.com>
2025-04-02 18:01:31 +0200

    add current state for singularity

--- ablations/set_env.sh
+++ ablations/set_env.sh
@@ -4,8 +4,13 @@ export HF_DATASETS_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
 export HF_HUB_OFFLINE=1
 
-# export OpenLLM_OUTPUT=$ALL_CCFRSCRATCH/OpenLLM-BPI-output
-# export OpenLLM_DATA=$ALL_CCFRSCRATCH/preprocessed_data/Lucie/lucie_tokens_65k_grouped
+# module purge
+# module load arch/h100 nemo/2.1.0
 
 module purge
-module load arch/h100 nemo/2.1.0
+module load anaconda-py3/2023.09 
+module load cuda/12.4.1
+module load gcc/11.3.1
+module load ffmpeg/6.1.1
+module load singularity/3.8.5
+conda activate /lustre/fsn1/projects/rech/qgz/commun/envs/nemo-env
