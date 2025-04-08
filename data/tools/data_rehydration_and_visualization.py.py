779636b6fbbb8819a0c5d82daa11f56737d20e77
Oligou <oliviergouvert@gmail.com>
2025-04-08 17:09:58 +0200

    clean visualization

--- data/tools/data_rehydration_and_visualization.py.py
+++ data/tools/data_rehydration_and_visualization.py.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import argparse
 from collections import OrderedDict
+import yaml 
 
 rehydratation_mapping = OrderedDict(
     zip(["1", "2", "3", "4", "5-100", "100-1000", "1000+"], [1, 2, 3, 3, 5, 8, 1])
@@ -31,6 +32,42 @@ def apply_rehydratation(df):
     df = df.sort_values('cluster_size')
     return df
 
+def load_metadata():
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Construct the full path to the YAML file relative to the script
+    yaml_file_path = os.path.join(script_dir, "../tokenization/datasets_to_tokenize.yaml")
+
+    # Load the YAML content
+    with open(yaml_file_path, "r") as f:
+        out = yaml.safe_load(f)
+    return out
+
+def hbar_plot(df, label_column, output_path):
+    df = df.groupby(label_column).agg({'total_tokens_rehydrated': 'sum'}).reset_index()
+    df = df[df['total_tokens_rehydrated'] > 0]
+    df['total_tokens_rehydrated'] /= 1e9
+
+    df = df.sort_values(by='total_tokens_rehydrated', ascending=True)
+
+    plt.figure(figsize=(10, 5))
+    plt.barh(df[label_column], df['total_tokens_rehydrated'])
+
+    # Add labels and customize the plot
+    plt.xscale('log')
+    plt.xlabel('Total Tokens (in Billions)')
+    plt.ylabel(label_column)
+    plt.title(f'Total Tokens by {label_column}')
+
+    # Optionally, add the labels on the bars
+    for index, value in enumerate(df['total_tokens_rehydrated']):
+        plt.text(value, index, f'{value:.2f} B', va='center', ha='left', fontsize=8, color='black')
+
+    # Save the plot
+    plt.tight_layout()
+    plt.savefig(output_path, bbox_inches='tight', dpi=300)
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
@@ -55,7 +92,9 @@ if __name__ == "__main__":
     df = pd.read_csv(data_path)
     df['total_tokens'] *= args.multiplier
     df = apply_rehydratation(df)
+    print(df.head(5))
     
+    ### Rehydratation upsampling
     for dataset, group in df.groupby('dataset'):
         if "fineweb2" in dataset:
             before = group['total_tokens'].sum()/ 1e9
@@ -65,3 +104,16 @@ if __name__ == "__main__":
             plt.yscale('log')
             plt.title(f"Tokens per cluster size in {dataset}\n Total tokens: from {before:.1f} B to {after:.1f} B (x{after/before:.1f})")
             plt.savefig(os.path.join(output_path, f"rehydration_{dataset}.png"))
+
+    ### Barplots
+    metadata = load_metadata()
+    metadata = pd.DataFrame(metadata['datasets'])
+    metadata = metadata[["name", "category", "language"]]
+    print(metadata.head(5))
+
+    df = df.merge(metadata, how="left", on='name')
+
+    # By dataset
+    hbar_plot(df, 'dataset', os.path.join(output_path, "hbar_dataset.png"))
+    hbar_plot(df, 'category', os.path.join(output_path, "hbar_category.png"))
+    hbar_plot(df, 'language', os.path.join(output_path, "hbar_language.png"))
