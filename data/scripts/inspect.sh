5d7db1951af13c73e6c83167fea7ae3c35604b0b
Oligou <oliviergouvert@gmail.com>
2025-04-08 15:20:07 +0200

    clean repo

--- data/scripts/inspect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PAGER='less -r'
-
-data_path=$DATA/fineweb2/data/fra_Latn/clusters/cluster_size:1000+
-data_path=$DATA/algebraic_stack/output
-
-python ~/datatrove/src/datatrove/tools/inspect_data.py $data_path -r jsonl -s 1 -l $SCRATCH/labeling_web.tmp 
--- data/scripts/merge_stats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find "${OpenLLM_OUTPUT}/datasets" -type d -name "stats" | while read data_path; do
-    output=$data_path/merged_stats.json
-    if [ ! -f "$output" ]; then
-        echo -e "\n---------------\nProcessing $data_path\n---------------"
-        python ~/datatrove/src/datatrove/tools/merge_stats.py "$data_path" --output "$output" 
-    fi
-done
