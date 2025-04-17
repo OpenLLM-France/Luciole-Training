984437abf246f6d3ce60049002e44f7f7fa47996
Audran Bert <bert.audran@gmail.com>
2025-04-17 11:49:55 +0200

    conversions test files

--- ablations/set_env.sh
+++ ablations/set_env.sh
@@ -3,5 +3,7 @@
 export OpenLLM_OUTPUT=$ALL_CCFRSCRATCH/OpenLLM-BPI-output
 export HF_HOME=$ALL_CCFRSCRATCH/.cache/huggingface
 
+export NVIDIA_PYTORCH_VERSION=24.03
+
 module purge
 module load arch/h100 nemo/2.1.0
