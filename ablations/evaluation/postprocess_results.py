b61c60f9b9c376983d2044c23f97cc811e8f89c7
Oligou <oliviergouvert@gmail.com>
2025-04-29 16:25:58 +0200

    change post process logic

--- ablations/evaluation/postprocess_results.py
+++ ablations/evaluation/postprocess_results.py
@@ -4,8 +4,9 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+import re
 
-def read_file(file):
+def read_json_results(file):
     with open(file, 'r') as file:
         data = json.load(file)
     model_name = data['config_general']['model_name']
@@ -14,24 +15,37 @@ def read_file(file):
     df['model_name'] = model_name
     return df
 
-def read_all_results(main_dir):
+def read_experiment_results(main_dir):
     main_dir = Path(main_dir)
-    json_files = main_dir.rglob('*.json')  # recursively finds all .json files
-    df = pd.concat([read_file(file) for file in json_files])
-    df['datamix'] = df['model_name'].str.extract(r'huggingface_checkpoints_datamix_(.*?)_4n_20b--')
+    experiment_name = main_dir.name
+
+    json_files = main_dir.rglob('results_*.json')  # recursively finds all .json files
+
+    dfs = []
+    for file in json_files:
+        df_part = read_json_results(file)
+        match = re.match(r'results_(.*)\.json', file.name)
+        df_part['timestamp'] = match.group(1) if match else None
+        dfs.append(df_part)
+    df = pd.concat(dfs, ignore_index=True)
+
+    # Fix date and deduplicate
+    df['timestamp'] = df['timestamp'].str.replace('T', ' ').str.replace(r'(\d{2})-(\d{2})-(\d{2}\.\d+)', r'\1:\2:\3', regex=True)
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df = df.sort_values('timestamp').drop_duplicates(subset=df.columns.difference(['timestamp']), keep='last')
+    # Post process some columns
+    df['experiment_name'] = experiment_name
     df['step'] = df['model_name'].str.extract(r'--step_([0-9.]+)-')[0].astype(float)
     df['samples'] = df['model_name'].str.extract(r'-consumed_samples_([0-9.]+)')[0].astype(float)
     df['tokens'] = df['samples'] * 2048 / 10**9
-    df['fr_prop'] = df['model_name'].str.extract(r'([0-9.]+)_fra_Latn')[0].astype(float)
-    df['en_prop'] = df['model_name'].str.extract(r'([0-9.]+)_eng_Latn')[0].astype(float)
     return df
 
 def plot_task(ax, df, task, metric, xlog=False):
     df = df[df['task'] == task]
     df = df.sort_values('tokens')
 
-    pivot_df = df.pivot(index='tokens', columns='datamix', values=metric)
-    stderr_df = df.pivot(index='tokens', columns='datamix', values=metric + '_stderr')
+    pivot_df = df.pivot(index='tokens', columns='experiment_name', values=metric)
+    stderr_df = df.pivot(index='tokens', columns='experiment_name', values=metric + '_stderr')
 
     for col in pivot_df.columns:
         mean = pivot_df[col].dropna()
@@ -52,7 +66,7 @@ def plot_task(ax, df, task, metric, xlog=False):
         ax.set_xscale('log')
 
 
-def plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir, title=None, xlog=False):
+def plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir=None, title=None, xlog=False):
     num_tasks = len(list_of_tasks_to_plot)
     num_plots = num_tasks + 1  # +1 for the legend
 
@@ -79,7 +93,7 @@ def plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir, title=None, xlog=F
     legend_ax.legend(
         legend_handles,
         legend_labels,
-        title="datamix",
+        title="Experiment name",
         loc="center",
         # prop={'size': 14},        
         # title_fontsize=16        
@@ -92,14 +106,17 @@ def plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir, title=None, xlog=F
     if title is not None:
         fig.suptitle(title)
     plt.tight_layout()
-    plt.savefig(output_dir)
+    if output_dir is not None:
+        plt.savefig(output_dir)
 
 if __name__=="__main__":
-    main_path = os.getenv("OpenLLM_OUTPUT")
-    main_dir = os.path.join(main_path, "ablations/evaluation/language_ablations")
-    df = read_all_results(main_dir)
+    dfs = []
+    main_path = os.getenv('OpenLLM_OUTPUT')
+    for expe_name in ['multi_base_4n_20b', 'multi_gallica_4n_20b', 'olmo2_4n_20b']:
+        path = os.path.join(f"{main_path}/ablations/train/lucie2_ablations/{expe_name}")
+        dfs.append(read_experiment_results(path))
+    df = pd.concat(dfs)
 
-    # English benchmarks
     list_of_tasks_to_plot = [
         ("helm|boolq|0", "pem"),
         ("lighteval|triviaqa|0", "qem"),
@@ -112,49 +129,4 @@ if __name__=="__main__":
         ("lighteval|openbookqa|0", "acc_norm"),
         ("lighteval|piqa|0", "acc_norm")    
     ]
-    output_dir = os.path.join(main_path, "ablations/evaluation/language_ablations/en.png")
-    plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir, title="English Tasks", xlog=False)
-
-    # French benchmarks
-    list_of_tasks_to_plot = [
-        ("lighteval|belebele_fra_Latn_cf|0", "acc_norm"), 
-        ("lighteval|mlmm_arc_fra_cf:challenge|0", "acc_norm_token"), 
-        ("lighteval|mlmm_arc_fra_cf:challenge|0", "acc_norm"), 
-        ("lighteval|mlmm_hellaswag_fra_cf|0", "acc_norm"), 
-        ("lighteval|xcodah_fra_cf|0", "acc_norm"), 
-        ("lighteval|xcsqa_fra_cf|0", "acc_norm"),     
-        ("lighteval|xnli2.0_fra_cf|0", "acc_norm"),     
-        ("lighteval|fquadv2_fra|0", "exact_match_fra_prefix"), 
-        ("lighteval|fquadv2_fra|0", "f1_fra"),     
-        ("lighteval|mintaka_fra|0", "exact_match_fra_prefix"), 
-        ("lighteval|mintaka_fra|0", "f1_fra"), 
-    ]
-    output_dir = os.path.join(main_path, "ablations/evaluation/language_ablations/fr.png")
-    plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir, title="French Tasks", xlog=False)
-
-    # French benchmarks
-    list_of_tasks_to_plot = [
-        ("lighteval|belebele_fra_Latn_cf|5", "acc_norm"), 
-        ("lighteval|mlmm_arc_fra_cf:challenge|5", "acc_norm"), 
-        ("lighteval|mlmm_hellaswag_fra_cf|5", "acc_norm"), 
-        ("lighteval|xcodah_fra_cf|5", "acc_norm"), 
-        ("lighteval|xcsqa_fra_cf|5", "acc_norm"),     
-        ("lighteval|xnli2.0_fra_cf|5", "acc_norm"),     
-        ("lighteval|fquadv2_fra|5", "exact_match_fra_prefix"), 
-        ("lighteval|fquadv2_fra|5", "f1_fra"),     
-        ("lighteval|mintaka_fra|5", "exact_match_fra_prefix"), 
-        ("lighteval|mintaka_fra|5", "f1_fra"), 
-    ]
-    output_dir = os.path.join(main_path, "ablations/evaluation/language_ablations/fr_5_shots.png")
-    plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir, title="French Tasks (5-shots)", xlog=False)
-
-    # French benchmarks
-    main_dir = os.path.join(main_path, "ablations/evaluation/language_ablations/fr_cf")
-    df = read_all_results(main_dir)
-    list_of_tasks_to_plot = [
-        ("lighteval|meta_mmlu_fra_cf:_average|0", "acc_norm"), 
-        ("lighteval|meta_mmlu_fra_cf:_average|0", "acc_norm_pmi"), 
-        ("lighteval|meta_mmlu_fra_cf:_average|0", "acc_norm_token"), 
-    ]
-    output_dir = os.path.join(main_path, "ablations/evaluation/language_ablations/fr_cf.png")
-    plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir, title="French MMLU", xlog=False)
+    plot_list_of_tasks(df, list_of_tasks_to_plot, output_dir="test.png", title=None, xlog=False)
