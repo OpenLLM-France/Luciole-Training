084409c71434a8705ea24b43ff6865d7137b161d
Oligou <oliviergouvert@gmail.com>
2025-05-15 11:30:04 +0200

    update dislabel geenration

--- data/synthetic_generation/README.md
+++ data/synthetic_generation/README.md
@@ -1,11 +1,24 @@
 ### Install env
 
 ```
+module load arch/h100 
+module load anaconda-py3/2024.06
+module load cuda/12.4.1
+<!-- module load gcc/11.4.1 -->
 conda create -n distilabel-env python=3.12
 conda activate distilabel-env
-pip install distilabel --upgrade
-pip install transformers
-pip install torch
+pip install --force-reinstall distilabel[vllm,hf-transformers] 
 pip install flash-attn --no-build-isolation
 pip install hf_xet
+pip install pynvml
+pip install bs4
+```
+
+### Use it
+
+```
+module load arch/h100 
+module load anaconda-py3/2024.06
+module load cuda/12.4.1
+conda activate distilabel-env
 ```
--- data/synthetic_generation/generate.py
+++ data/synthetic_generation/generate.py
@@ -1,7 +1,8 @@
-from distilabel.models import TransformersLLM
+from distilabel.models import TransformersLLM, vLLM
 from distilabel.pipeline import Pipeline
 from distilabel.steps.tasks import TextGeneration
 from distilabel.steps import LoadDataFromDicts, LoadDataFromDisk
+from distilabel.steps import StepResources
 
 import datasets
 import os
@@ -18,9 +19,24 @@ main_path = os.getenv("OpenLLM_OUTPUT")
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
+        "--model_name",
+        type=str,
+        default="/lustre/fsmisc/dataset/HuggingFace_Models/meta-llama/Llama-3.1-8B-Instruct",
+    )
+    argparser.add_argument(
+        "--data_path",
+        type=str,
+        default=os.path.join(main_path, "data/raw_datasets_ablation/fineweb2/data/fra_Latn/train"),
+    )
+    argparser.add_argument(
         "--prompt",
         type=str,
         default="prompt/en.txt",
+    )
+    argparser.add_argument(
+        "--output_dir",
+        type=str,
+        default="out",
     )
     argparser.add_argument(
         "--disable_thinking",
@@ -28,30 +44,33 @@ if __name__ == "__main__":
         help="Disable the thinking process."
     )
     args = argparser.parse_args()
+    model_name = args.model_name
+    data_path = args.data_path
+    output_dir = args.output_dir
+    os.makedirs(output_dir, exist_ok=True)
 
     with open(args.prompt, 'r', encoding='utf-8') as file:
         prompt = file.read()
 
     # Preprocess dataset
-    dataset = datasets.load_dataset(
-        os.path.join(main_path, "data/raw_datasets/fineweb2/data/fra_Latn/train"), split='train'
-    )
+    dataset = datasets.load_dataset(data_path, split="train").select(range(100))  # Select a subset for testing
     dataset = dataset.map(
-        lambda x: {"instruction": prompt.replace('<extrait>', x["text"])},
+        lambda x: {"instruction": prompt.replace('<text>', x["text"])},
         remove_columns=dataset.column_names,
     )
-    dataset = dataset.select(range(5))
 
     # Define the pipeline
     with Pipeline(name="annotation") as pipeline:
         generation = TextGeneration(
             llm=TransformersLLM(
-                model="Qwen/Qwen3-0.6B",
+                model=model_name,
                 generation_kwargs={"temperature": 0.8, "max_new_tokens": 512},
                 chat_template = chat_template if args.disable_thinking else None,
-            )
+            ),
+            input_batch_size=8,
+            # resources=StepResources(replicas=1, cpus=1, gpus=4)
         )
 
     distiset = pipeline.run(dataset=dataset, use_cache=False)
 
-    distiset['default']['train'].to_pandas().to_csv("data.csv")
+    distiset['default']['train'].to_json(os.path.join(output_dir, "data.jsonl"))
