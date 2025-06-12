a39460624d80c7eb8927e32c9951091736a58258
Oligou <oliviergouvert@gmail.com>
2025-06-12 09:15:30 +0200

    update inspect script

--- data/tools/inspect.sh
+++ data/tools/inspect.sh
@@ -2,6 +2,6 @@
 
 export PAGER='less -r'
 
-data_path=$OpenLLM_OUTPUT/data/raw_datasets/fineweb2/data/fra_Latn/clusters/cluster_size:1000+
+data_path=$1
 
 python ~/datatrove/src/datatrove/tools/inspect_data.py $data_path -r jsonl -s 1 -l $SCRATCH/labeling_web.tmp 
