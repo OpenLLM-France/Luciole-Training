57716083f329ad60105efbd14f8fdf22758e9a73
Oligou <oliviergouvert@gmail.com>
2025-06-16 14:53:08 +0200

    update math extraction, add finemath prompts

--- data/prepare_cache.py
+++ data/prepare_cache.py
@@ -1,5 +1,8 @@
 from datatrove.io import cached_asset_path_or_download
+from pathlib import Path
+from huggingface_hub import hf_hub_url
 
+# Fasttext
 MODEL_URL = "https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin"
 MODEL_SUBFOLDER = "ft176"
 
@@ -12,15 +15,13 @@ model_file = cached_asset_path_or_download(
 print(model_file)
 
 # CCNET
-# from pathlib import Path
-# from huggingface_hub import hf_hub_url
-# MODEL_REPO = "edugp/kenlm"
+MODEL_REPO = "edugp/kenlm"
 # /lustre/fswork/projects/rech/fwx/commun/.cache/huggingface/assets/datatrove/default/default/
-# model_dataset = "wikipedia"
-# for model_name in ['fr', 'es']:
-#     path = cached_asset_path_or_download(
-#         hf_hub_url(MODEL_REPO, str(Path(model_dataset, f"{model_name}.arpa.bin")))
-#     )
-#     path = cached_asset_path_or_download(
-#         hf_hub_url(MODEL_REPO, str(Path(model_dataset, f"{model_name}.sp.model")))
-#     )
+model_dataset = "wikipedia"
+for model_name in ["fr"]:
+    path = cached_asset_path_or_download(
+        hf_hub_url(MODEL_REPO, str(Path(model_dataset, f"{model_name}.arpa.bin")))
+    )
+    path = cached_asset_path_or_download(
+        hf_hub_url(MODEL_REPO, str(Path(model_dataset, f"{model_name}.sp.model")))
+    )
