24463f166ba46c2d174e4328f867cfb46c9ee74f
Oligou <oliviergouvert@gmail.com>
2025-06-19 18:00:31 +0200

    improve american stroies and common pile (add prefix)

--- data/processing/american_stories.py
+++ data/processing/american_stories.py
@@ -1,10 +1,9 @@
-import os
-
 from utils import create_parser, parse_args, create_executor, add_sampler_filter
-
 from datatrove.pipeline.readers import HuggingFaceDatasetReader
 from datatrove.pipeline.writers import JsonlWriter
 from datatrove.pipeline.filters import PerplexityFilter
+from datatrove.pipeline.filters.prefix_formatter import PrefixFormatter
+import os
 
 TASKS = 1
 SUPPORTED_YEARS = [
@@ -21,12 +20,31 @@ SUPPORTED_YEARS = [
     "1793",
 ] + [str(year) for year in range(1796, 1964 + 1)]
 
+
+def format_newspaper(doc):
+    infos = []
+    newspaper_name = doc.metadata.get("newspaper_name")
+    if newspaper_name:
+        infos.append(f"Newspaper: {newspaper_name}")
+    ccnet_perplexity_en = doc.metadata.get("ccnet_perplexity_en")
+    if ccnet_perplexity_en:
+        if ccnet_perplexity_en < 500:
+            infos.append("OCR quality: high")
+        elif ccnet_perplexity_en < 1000:
+            infos.append("OCR quality: medium")
+        else:
+            infos.append("OCR quality: low")
+    return infos
+
+
 if __name__ == "__main__":
     parser = create_parser()
     args = parse_args(parser)
     DATA_PATH = args.data_path
 
     dataset_name = "american_stories"
+    if args.local:
+        SUPPORTED_YEARS = ["1770"]
 
     for year in SUPPORTED_YEARS:
         year = str(year)
@@ -49,6 +67,10 @@ if __name__ == "__main__":
                 min_ppl=10,
                 max_ppl=2000,
                 exclusion_writer=JsonlWriter(f"{output_path}/removed/ppl/{year}"),
+            ),
+            PrefixFormatter(
+                date_format="%Y-%m-%d",
+                additionnal_formatting=lambda doc: format_newspaper(doc),
             ),
             JsonlWriter(f"{output_path}/data/{year}"),
         ]
