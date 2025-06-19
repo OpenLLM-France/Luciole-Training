ce64f357b353b0115d94ef5b00a048eef93acc31
Oligou <oliviergouvert@gmail.com>
2025-06-19 18:33:07 +0200

    add wikimedia

---
+++ data/processing/wikimedia.py
@@ -0,0 +1,68 @@
+from utils import create_parser, parse_args, create_executor, add_sampler_filter
+
+from datatrove.pipeline.readers import HuggingFaceDatasetReader
+from datatrove.pipeline.writers import JsonlWriter
+
+LANGUAGES = [
+    "ar",
+    "br",
+    "ca",
+    "co",
+    "de",
+    "en",
+    "es",
+    "eu",
+    "fr",
+    "frp",
+    "it",
+    "nl",
+    "oc",
+    "pcd",
+    "pt",
+]
+
+if __name__ == "__main__":
+    parser = create_parser()
+    parser.add_argument(
+        "--language",
+        type=str,
+        default="fr",
+        help="Language to load",
+    )
+    parser.add_argument(
+        "--all",
+        action="store_true",
+    )
+    args = parse_args(parser)
+    DATA_PATH = args.data_path
+
+    if args.all:
+        languages = LANGUAGES
+    else:
+        languages = [args.language]
+
+    for language in languages:
+        if language in ["ar", "ca", "de", "en", "es", "eu", "fr", "it", "nl", "pt"]:
+            tasks = 50
+        else:
+            tasks = 5
+
+        pipeline = [
+            HuggingFaceDatasetReader(
+                "OpenLLM-France/wikimedia",
+                {"name": language, "split": "train"},
+                streaming=True,
+            ),
+            JsonlWriter(f"{DATA_PATH}/wikimedia/{language}/data"),
+        ]
+        add_sampler_filter(pipeline, args.sample_rate)
+
+        main_processing_executor = create_executor(
+            pipeline,
+            local=args.local,
+            logging_dir=f"{DATA_PATH}/wikimedia/{language}/logs",
+            job_name="wikimedia",
+            tasks=tasks,
+        )
+
+        main_processing_executor.run()
