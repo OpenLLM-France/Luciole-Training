ff5c0c19607aaefdcf40f31197e08f574dee1257
Oligou <oliviergouvert@gmail.com>
2025-06-19 18:00:31 +0200

    improve american stroies and common pile (add prefix)

--- data/processing/common_pile.py
+++ data/processing/common_pile.py
@@ -3,15 +3,48 @@ from utils import create_parser, parse_args, create_executor, add_sampler_filter
 from datatrove.pipeline.readers import HuggingFaceDatasetReader
 from datatrove.pipeline.writers import JsonlWriter
 from datatrove.pipeline.filters import LanguageFilter, LambdaFilter
+from datatrove.pipeline.filters.prefix_formatter import PrefixFormatter
 
-LANGUAGE_METADATA = [
-    "upsto_filtered",
+COMMON_PILE_DATASETS = [
+    "peS2o_filtered",
+    "uspto_filtered",
+    "biodiversity_heritage_library_filtered",
+    "caselaw_access_project_filtered",
+    "pubmed_filtered",
+    "libretexts_filtered",
     "project_gutenberg_filtered",
+    "doab_filtered",
     "library_of_congress_filtered",
+    "pressbooks_filtered",
     "pre_1929_books_filtered",
+    "regulations_filtered",
     "uk_hansard_filtered",
+    "usgpo_filtered",
+    "stackexchange_filtered",
+    "ubuntu_irc_filtered",
+    "public_domain_review_filtered",
+    "foodista_filtered",
+    "youtube_filtered",
     "data_provenance_initiative_filtered",
+    "python_enhancement_proposals_filtered",
 ]
+
+
+def additionnal_formatting(doc):
+    infos = []
+    channel = doc.metadata.get("channel")
+    if channel:
+        infos.append(f"Channel: {channel}")
+    title = doc.metadata.get("title")
+    if title and title.lower() not in doc.text[:200].lower():
+        infos.append(f"Title: {title}")
+    extfieldsofstudy = doc.metadata.get("s2fieldsofstudy")
+    if extfieldsofstudy:
+        extfieldsofstudy = ", ".join(extfieldsofstudy)
+        if extfieldsofstudy:
+            infos.append(f"Fields: {extfieldsofstudy}")
+    return infos
+
 
 if __name__ == "__main__":
     parser = create_parser()
@@ -19,76 +52,76 @@ if __name__ == "__main__":
         "--name",
         type=str,
         default=None,
-        choices=[
-            "peS2o_filtered",
-            "upsto_filtered",
-            "biodiversity_heritage_library_filtered",
-            "caselaw_access_project_filtered",
-            "pubmed_filtered",
-            "libretexts_filtered",
-            "project_gutenberg_filtered",
-            "doab_filtered",
-            "library_of_congress_filtered",
-            "pressbooks_filtered",
-            "pre_1929_books_filtered",
-            "regulations_filtered",
-            "uk_hansard_filtered",
-            "usgpo_filtered",
-            "stackexchange_filtered",
-            "ubuntu_irc_filtered",
-            "public_domain_review_filtered",
-            "foodista_filtered",
-            "youtube_filtered",
-            "data_provenance_initiative_filtered",
-            "python_enhancement_proposals_filtered",
-        ],
+        nargs="+",
+        choices=COMMON_PILE_DATASETS,
         help="Subset to load",
+    )
+    parser.add_argument(
+        "--all",
+        action="store_true",
     )
 
     args = parse_args(parser)
     DATA_PATH = args.data_path
 
-    name = args.name
-
-    if name == "ubuntu_irc_filtered":
-        language_filter = [
-            LanguageFilter(
-                label_only=True,
-                keep_top_pairs_threshold=1,
-            ),
-            LambdaFilter(
-                lambda doc: doc.metadata["language"]
-                in ["en", "fr", "it", "de", "es", "ar", "pt", "nl"],
-                exclusion_writer=JsonlWriter(
-                    f"{DATA_PATH}/common_pile/{name}/removed/language"
-                ),
-            ),
-        ]
+    if args.all:
+        names = COMMON_PILE_DATASETS
     else:
-        language_filter = [
-            LanguageFilter(
-                languages=["en", "fr", "it", "de", "es", "ar", "pt", "nl"],
-                language_threshold=0.65,
-                keep_top_pairs_threshold=1,
+        names = args.name
+
+    for name in names:
+        print(f"\nProcessing {name}...")
+        if name == "ubuntu_irc_filtered":
+            language_filter = [
+                LanguageFilter(
+                    label_only=True,
+                    keep_top_pairs_threshold=1,
+                ),
+                LambdaFilter(
+                    lambda doc: doc.metadata["language"]
+                    in ["en", "fr", "it", "de", "es", "ar", "pt", "nl"],
+                    exclusion_writer=JsonlWriter(
+                        f"{DATA_PATH}/common_pile/{name}/removed/language"
+                    ),
+                ),
+            ]
+        else:
+            language_filter = [
+                LanguageFilter(
+                    languages=["en", "fr", "it", "de", "es", "ar", "pt", "nl"],
+                    language_threshold=0.65,
+                    keep_top_pairs_threshold=1,
+                ),
+            ]
+
+        if name == "public_domain_review_filtered":
+            date_keys = None  # Date already in the text
+        else:
+            date_keys = ["created", "date", "published_time"]
+        url_keys = ["url", "oa_url", "item_url"]
+
+        pipeline = [
+            HuggingFaceDatasetReader(
+                f"common-pile/{name}",
+                {"split": "train"},
+                streaming=True,
             ),
+            *language_filter,
+            PrefixFormatter(
+                date_keys=date_keys,
+                url_keys=url_keys,
+                infer_date_format=True,
+                additionnal_formatting=additionnal_formatting,
+            ),
+            JsonlWriter(f"{DATA_PATH}/common_pile/{name}/data"),
         ]
+        add_sampler_filter(pipeline, args.sample_rate)
 
-    pipeline = [
-        HuggingFaceDatasetReader(
-            f"common-pile/{name}",
-            {"split": "train"},
-            streaming=True,
-        ),
-        *language_filter,
-        JsonlWriter(f"{DATA_PATH}/common_pile/{name}/data"),
-    ]
-    add_sampler_filter(pipeline, args.sample_rate)
+        main_processing_executor = create_executor(
+            pipeline,
+            local=args.local,
+            logging_dir=f"{DATA_PATH}/common_pile/{name}/logs",
+            job_name=name,
+        )
 
-    main_processing_executor = create_executor(
-        pipeline,
-        local=args.local,
-        logging_dir=f"{DATA_PATH}/common_pile/{name}/logs",
-        job_name=name,
-    )
-
-    main_processing_executor.run()
+        main_processing_executor.run()
