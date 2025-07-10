30c5bcf9ef3d47dda06bca81b2ac23c443a34dc1
Oligou <oliviergouvert@gmail.com>
2025-07-10 10:10:49 +0200

    add links to decontamination

--- data/processing/decontamination_index.py
+++ data/processing/decontamination_index.py
@@ -2,6 +2,28 @@ from utils import create_parser, parse_args, create_executor
 from datatrove.pipeline.decont import NGramsDecontConfig, NGramsDecontIndexer
 import os
 
+"""
+Decontamination Indexing Script
+List of tasks available on lighteval:
+- https://huggingface.co/docs/lighteval/available-tasks
+- https://github.com/huggingface/lighteval/blob/main/community_tasks/french_evals.py
+- https://github.com/huggingface/lighteval/blob/main/community_tasks/arabic_evals.py
+- https://github.com/huggingface/lighteval/blob/main/src/lighteval/tasks/multilingual/tasks.py
+
+Languages we are interested n:
+- English (en)
+- French (fr)
+- Arabic (ar)
+- Spanish (es)
+- Portuguese (pt)
+- Italian (it)
+- German (de)
+- Dutch (nl)
+- Basque (eu)
+- Catalan (ca)
+- French regional languages 
+"""
+
 MAIN_PATH = os.getenv("OpenLLM_OUTPUT")
 if not MAIN_PATH:
     raise RuntimeError("Environment variable 'OpenLLM_OUTPUT' is not set or is empty.")
