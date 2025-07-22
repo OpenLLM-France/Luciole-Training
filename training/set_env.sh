69a86279bb07d86dda5a0fe71f4ebb7912c1c501
Audran Bert <bert.audran@gmail.com>
2025-07-22 14:30:38 +0200

    upd env version + remove minimal

--- training/set_env.sh
+++ training/set_env.sh
@@ -4,4 +4,4 @@ export OpenLLM_OUTPUT=$qgz_ALL_CCFRSCRATCH/OpenLLM-BPI-output
 export HF_HOME=$qgz_ALL_CCFRSCRATCH/.cache/huggingface
 
 module purge
-module load arch/h100 nemo/2.1.0
+module load arch/h100 nemo/2.3.1
