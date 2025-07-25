fbb1304a41b638ea6c97aaecd23808d71f12625e
Oligou <oliviergouvert@gmail.com>
2025-07-25 17:13:31 +0200

    update eng web data

--- data/processing/fineweb_edu.py
+++ data/processing/fineweb_edu.py
@@ -1,27 +1,20 @@
-import os
-
 from utils import create_parser, parse_args, create_executor, add_sampler_filter
-
+from web_utils import get_robot_filter
 from datatrove.pipeline.readers import ParquetReader
 from datatrove.pipeline.writers import JsonlWriter
-
-# FineWeb edu is already on JZ: $DSDIR/HuggingFace/fineweb/data
-# up to CC-MAIN-2024-10
-# new dumps are available
 
 if __name__ == "__main__":
     parser = create_parser()
     args = parse_args(parser)
     DATA_PATH = args.data_path
 
-    dataset_name = "fineweb_edu"
-    output_path = os.path.join(DATA_PATH, dataset_name)
-
+    ### LOAD
     pipeline = [
         ParquetReader(
             "hf://datasets/HuggingFaceFW/fineweb-edu", glob_pattern="data/*/*.parquet"
         ),
-        JsonlWriter(f"{output_path}/output"),
+        get_robot_filter(output_path=f"{DATA_PATH}/fineweb_edu/removed"),
+        JsonlWriter(f"{DATA_PATH}/fineweb_edu/data"),
     ]
     add_sampler_filter(pipeline, args.sample_rate)
 
@@ -29,9 +22,9 @@ if __name__ == "__main__":
         pipeline,
         local=args.local,
         debug=args.debug,
-        logging_dir=f"{output_path}/logs",
-        job_name=dataset_name,
+        logging_dir=f"{DATA_PATH}/fineweb_edu/logs",
+        job_name="fw-edu",
+        tasks=100,
+        max_array_size=50,
     )
-    main_processing_executor.tasks = 100
-
     main_processing_executor.run()
