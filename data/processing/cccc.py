87c69c4bd1ef575efc335263d04ca46337f32cce
Oligou <oliviergouvert@gmail.com>
2025-07-30 16:44:39 +0200

    Update processing scripts; update figures

--- data/processing/cccc.py
+++ data/processing/cccc.py
@@ -1,6 +1,6 @@
 from utils import create_parser, parse_args, create_executor, add_sampler_filter
 from web_utils import get_web_pipeline
-from datatrove.pipeline.readers import ParquetReader
+from datatrove.pipeline.readers import HuggingFaceDatasetReader
 from datatrove.pipeline.writers import JsonlWriter
 
 if __name__ == "__main__":
@@ -8,19 +8,20 @@ if __name__ == "__main__":
     args = parse_args(parser)
     DATA_PATH = args.data_path
 
-    ### LOAD
     pipeline = [
-        ParquetReader(
-            "hf://datasets/HuggingFaceFW/fineweb-edu", glob_pattern="data/*/*.parquet"
+        HuggingFaceDatasetReader(
+            "common-pile/cccc_filtered",
+            {"split": "train"},
+            streaming=True,
         ),
         *get_web_pipeline(
             "en",
-            f"{DATA_PATH}/fineweb_edu_filtered",
+            f"{DATA_PATH}/cccc_filtered",
             do_edu=False,
             do_pii=True,
             do_decont=False,
         ),
-        JsonlWriter(f"{DATA_PATH}/fineweb_edu_filtered/data"),
+        JsonlWriter(f"{DATA_PATH}/cccc_filtered/data"),
     ]
     add_sampler_filter(pipeline, args.sample_rate)
 
@@ -28,10 +29,8 @@ if __name__ == "__main__":
         pipeline,
         local=args.local,
         debug=args.debug,
-        logging_dir=f"{DATA_PATH}/fineweb_edu_filtered/logs",
-        job_name="fw-edu",
-        tasks=100,
-        max_array_size=50,
-        time="20:00:00",
+        logging_dir=f"{DATA_PATH}/cccc_filtered/logs",
+        job_name="cccc",
+        tasks=10,
     )
     main_processing_executor.run()
--- data/processing/gallica.py
+++ data/processing/gallica.py
@@ -1,16 +1,10 @@
 import os
-
 from utils import create_parser, parse_args, create_executor, add_sampler_filter
-
 from datatrove.pipeline.readers import ParquetReader
 from datatrove.pipeline.writers import JsonlWriter
 from datatrove.pipeline.filters.prefix_formatter import PrefixFormatter
-from datatrove.pipeline.filters import ExtremeTokenizerFilter
+from datatrove.pipeline.filters import ExtremeTokenizerFilter, LambdaFilter
 from functools import partial
-
-# Tried GopherQualityFilter with fineweb-2 config file for french
-# It took 7 minutes and 47 secondes for 1k documents...
-#     Stats: {total: 538, dropped: 355, dropped_gopher_below_alpha_threshold: 328, dropped_gopher_long_doc: 26, forwarded: 183, doc_len: 17142473 [min=2233, max=592330, 93674.72±96825/doc], dropped_gopher_below_avg_threshold: 1}
 
 mapping = {
     "monographies": "PleIAs/French-PD-Books",
@@ -22,9 +16,6 @@ def additionnal_formatting(doc, name):
     import re
 
     out = {}
-    # ocr = doc.metadata.get("ocr")
-    # if ocr:
-    #     out["ocr"] = doc.metadata.get("ocr")
     author = doc.metadata.get("author")
     if author and author != "None":
         out["author"] = doc.metadata.get("author")
@@ -64,24 +55,36 @@ if __name__ == "__main__":
             text_key="complete_text",
         ),
         ExtremeTokenizerFilter(
-            tokenizer_name_or_path="OpenLLM-BPI/tokenizer_128k-arab-regional",
-            max_token_per_char=0.4,
+            tokenizer_name_or_path="OpenLLM-BPI/tokenizer_128k-arab-regional_v2",
+            max_token_per_char=0.38,
+            normalize_digits=False,
             mode="CHUNKS",
+            min_length=1000,
+            max_length=2000,
+            separator=("\n", ". ", ", ", " "),
             replace_span="\n\n[...]\n\n",
             removed_spans_in_metadata=False,  # FOR DEBUGGING only
             exclusion_writer=JsonlWriter(f"{output_path}/removed/extreme_tokenizer"),
+        ),
+        LambdaFilter(
+            lambda doc: len(doc.text.split()) > 50,
+            exclusion_writer=JsonlWriter(
+                f"{output_path}/removed/too_short_doc",
+            ),
         ),
         PrefixFormatter(
             date_keys=[],
             additionnal_formatting=partial(additionnal_formatting, name=args.name),
             prefix_pipeline={
-                "author": "Author",
-                "title": "Title",
+                "author": "Auteur",
+                "title": "Titre",
                 "date": "Date",
-                # "ocr": "OCR score"
             },
         ),
-        JsonlWriter(f"{output_path}/data"),
+        JsonlWriter(
+            f"{output_path}/data",
+            output_filename="${rank}.jsonl.gz",
+        ),
     ]
     add_sampler_filter(pipeline, args.sample_rate)
 
@@ -91,5 +94,6 @@ if __name__ == "__main__":
         debug=args.debug,
         logging_dir=f"{output_path}/logs",
         job_name=args.name,
+        # depends_job_id="1602437:1602429:1602423:1602449"
     )
     main_processing_executor.run()
