49737e2cd1fba1d750ebda4f836afa7399b5399f
Oligou <oliviergouvert@gmail.com>
2025-07-31 09:36:11 +0200

    sort merged dataset; clean script

--- data/tokenization/run/concat_tokens.py
+++ data/tokenization/run/concat_tokens.py
@@ -1,6 +1,7 @@
 import os
 from nemo_patch import indexed_dataset
 
+
 def get_filename_without_extension(filename):
     """
     Returns the filename without its extension.
@@ -9,6 +10,7 @@ def get_filename_without_extension(filename):
         filename = filename[:-4]
     return filename
 
+
 if __name__ == "__main__":
     import argparse
 
@@ -16,10 +18,22 @@ if __name__ == "__main__":
         description="Concatenate MMap Indexed datasets",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("inputs", type=str, nargs="+", help="Input indexed dataset filenames (with extension .bin or without extension)")
-    parser.add_argument("output", type=str, help="Output filenames prefix (with extension .bin or without extension)")
     parser.add_argument(
-        "--vocab_size", type=int, default=128000, help="Vocabulary size (is it larger or not than 65500?)"
+        "inputs",
+        type=str,
+        nargs="+",
+        help="Input indexed dataset filenames (with extension .bin or without extension)",
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        help="Output filenames prefix (with extension .bin or without extension)",
+    )
+    parser.add_argument(
+        "--vocab_size",
+        type=int,
+        default=128000,
+        help="Vocabulary size (is it larger or not than 65500?)",
     )
     args = parser.parse_args()
 
@@ -30,8 +44,12 @@ if __name__ == "__main__":
     assert len(inputs)
     assert output
     assert output not in inputs
-    assert all(os.path.exists(input_file + ".bin") for input_file in inputs), "All input .bin files must exist."
-    assert all(os.path.exists(input_file + ".idx") for input_file in inputs), "All input .idx files must exist."
+    assert all(
+        os.path.exists(input_file + ".bin") for input_file in inputs
+    ), "All input .bin files must exist."
+    assert all(
+        os.path.exists(input_file + ".idx") for input_file in inputs
+    ), "All input .idx files must exist."
 
     output_bin_file = output + ".bin"
     output_idx_file = output + ".idx"
@@ -40,9 +58,11 @@ if __name__ == "__main__":
         os.makedirs(os.path.dirname(output_bin_file))
 
     # Aggregate data and write output bin
-    builder = indexed_dataset.make_builder(output_bin_file, impl="mmap", vocab_size=vocab_size)
+    builder = indexed_dataset.make_builder(
+        output_bin_file, impl="mmap", vocab_size=vocab_size
+    )
 
-    try:        
+    try:
         for path in inputs:
             builder.merge_file_(path)
             # dataset = indexed_dataset.MMapIndexedDataset(path)
@@ -54,4 +74,3 @@ if __name__ == "__main__":
         raise err
 
     builder.finalize(output_idx_file)
-
