ff7fc522674e6d4d93be2043552423c272651ff6
Oligou <oliviergouvert@gmail.com>
2025-10-21 17:25:16 +0200

    update run_all_task

--- training/evaluation/run_all_tasks.sh
+++ training/evaluation/run_all_tasks.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-module load anaconda-py3/2024.06
-conda activate eval-env
 
-python evaluate_experiment.py "$@" tasks/en.txt 
-python evaluate_experiment.py "$@" tasks/math.txt --command vllm
-python evaluate_experiment.py "$@" tasks/fr_leaderboard.txt --command vllm
+python evaluate_experiment.py "$@" tasks/gsm8k.txt 
+python evaluate_experiment.py "$@" tasks/en.txt
+# python evaluate_experiment.py "$@" tasks/math.txt --command vllm
 python evaluate_experiment.py "$@" tasks/fr.txt --command vllm --custom_tasks multilingual --max_samples 1000
 python evaluate_experiment.py "$@" tasks/multilingual.txt --command vllm --custom_tasks multilingual --max_samples 1000
