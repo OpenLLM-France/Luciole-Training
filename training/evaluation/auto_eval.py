bb81d7e6ece147c27797de63627c6cd9777fadff
Oligou <oliviergouvert@gmail.com>
2025-11-21 16:12:13 +0100

    add plot sent to email

--- training/evaluation/auto_eval.py
+++ training/evaluation/auto_eval.py
@@ -43,14 +43,62 @@ module purge
 module load anaconda-py3/2024.06
 conda activate eval-env
 
-export OpenLLM_OUTPUT=$qgz_ALL_CCFRSCRATCH/OpenLLM-BPI-output
-export HF_HOME=$qgz_ALL_CCFRSCRATCH/.cache/huggingface
-export HF_HUB_OFFLINE=1
-
 python evaluate_experiment.py {experiment_path} tasks/gsm8k.txt --multiple_of {multiple_of} --command vllm 
 python evaluate_experiment.py {experiment_path} tasks/en.txt --multiple_of {multiple_of} --command vllm
 python evaluate_experiment.py {experiment_path} tasks/fr.txt --multiple_of {multiple_of} --command vllm --custom_tasks multilingual --max_samples 1000
 python evaluate_experiment.py {experiment_path} tasks/multilingual.txt --multiple_of {multiple_of} --command vllm --custom_tasks multilingual --max_samples 1000
+"""
+
+SBATCH_PLOT_TEMPLATE = """#!/bin/bash
+#SBATCH --job-name=plot
+#SBATCH --output={log_dir}/log_%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=02:00:00
+#SBATCH --hint=nomultithread
+#SBATCH --qos=qos_cpu-dev
+#SBATCH --account=qgz@cpu
+#SBATCH --partition=prepost
+
+export OpenLLM_OUTPUT=$qgz_ALL_CCFRSCRATCH/OpenLLM-BPI-output
+export HF_HOME=$qgz_ALL_CCFRSCRATCH/.cache/huggingface
+export HF_HUB_OFFLINE=1
+
+module purge
+module load anaconda-py3/2024.06
+conda activate eval-env
+
+python plot_results.py {experiment_path} --group all en fr multilingual --output_path {experiment_path}/figs
+
+# Variables
+TO="{email}"
+EXPERIMENT_PATH="{experiment_path}"
+EXPERIMENT_NAME=$(basename "$EXPERIMENT_PATH")
+SUBJECT="Evaluation $EXPERIMENT_NAME"
+BODY="Please find the attachments."
+FOLDER="{experiment_path}/figs"
+
+# Check that TO is not empty
+if [[ -z "$TO" ]]; then
+    echo "Error: TO is empty, aborting."
+    exit 1
+fi
+
+# Build the attachment parameters for mailx
+ATTACHMENTS=""
+for f in "$FOLDER"/*; do
+    # Skip if folder is empty or no files match
+    [[ -e "$f" ]] || continue
+    ATTACHMENTS="$ATTACHMENTS -a \"$f\""
+done
+
+if [[ -z "$ATTACHMENTS" ]]; then
+    echo "Warning: no files to attach in $FOLDER."
+fi
+
+# Send email
+eval echo "$BODY" | mailx -s "$SUBJECT" $ATTACHMENTS "$TO"
 """
 
 
@@ -97,6 +145,26 @@ def launch_evaluation(experiment_path, multiple_of=1, dependency_job_id=None):
     with open(job_filename, "w") as f:
         f.write(job_script)
 
+    command = ["sbatch", "--parsable", str(job_filename)]
+    command.insert(1, f"--dependency=afterok:{dependency_job_id}")
+    result = subprocess.run(command, check=True, capture_output=True, text=True)
+    job_id = result.stdout.strip()
+    return job_id
+
+
+def launch_plot(experiment_path, email="", dependency_job_id=None):
+    print(f"Launching plot for {experiment_path}")
+    job_dir = Path(experiment_path) / "evaluation"
+    job_dir.mkdir(parents=True, exist_ok=True)
+
+    job_script = SBATCH_PLOT_TEMPLATE.format(
+        log_dir=job_dir / "slurm_logs", experiment_path=experiment_path, email=email
+    )
+
+    job_filename = job_dir / "plot_job.slurm"
+    with open(job_filename, "w") as f:
+        f.write(job_script)
+
     subprocess.run(
         ["sbatch", f"--dependency=afterok:{dependency_job_id}", str(job_filename)],
         check=True,
@@ -116,6 +184,12 @@ if __name__ == "__main__":
         default=None,
         help="Begin time for slurm job (e.g., now+30minutes)",
     )
+    parser.add_argument(
+        "--email",
+        type=str,
+        default="",
+        help="Email to send final figures",
+    )
     args = parser.parse_args()
 
     # Launch conversion job
@@ -124,6 +198,11 @@ if __name__ == "__main__":
     )
 
     # Launch evaluation job
-    launch_evaluation(
+    evaluation_job_id = launch_evaluation(
         args.experiment_path, args.multiple_of, dependency_job_id=conversion_job_id
+    )
+
+    # Plot
+    launch_plot(
+        args.experiment_path, dependency_job_id=evaluation_job_id, email=args.email
     )
