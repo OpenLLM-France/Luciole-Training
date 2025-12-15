44614d284c4666eaabb3857824137566e29f639d
oligou <oliviergouvert@gmail.com>
2025-12-15 17:43:38 +0100

    add fill in the blank formulation for idiomatic expression

--- training/evaluation/custom_benchmarks/idiomatic_expressions.py
+++ training/evaluation/custom_benchmarks/idiomatic_expressions.py
@@ -1,4 +1,6 @@
 from lighteval.metrics.dynamic_metrics import LogLikelihoodAccMetric
+from lighteval.metrics.metrics import Metrics
+from lighteval.tasks.requests import Doc
 from lighteval.metrics.normalizations import LogProbCharNorm, LogProbTokenNorm
 from lighteval.tasks.default_prompts import LETTER_INDICES
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
@@ -12,18 +14,12 @@ from lighteval.tasks.templates.utils.formulation import (
 from lighteval.utils.language import Language
 from functools import partial
 
-all_qa_formulations = [MCFFormulation(), CFFormulation(), HybridFormulation()]
+TASKS_TABLE = []
 
 SUBSETS = ["different", "word by word", "similar"]
 
 
-def process_line(line, use_context, use_instruction):
-    if use_instruction:
-        question = (
-            "Quelle est l'expression idiomatique parmi les 4 propositions suivantes?"
-        )
-    else:
-        question = ""
+def get_choices_and_gold_from_line(line, use_context=False):
     if use_context:
         sentence_context = line["French with context"]
     else:
@@ -39,7 +35,13 @@ def process_line(line, use_context, use_instruction):
         if line["answer"].isdigit()
         else LETTER_INDICES.index(line["answer"])
     )
+    return choices, gold_idx
 
+
+#### Idiomatic Expressions MCQ tasks
+def process_line(line, use_context):
+    question = "Quelle est l'expression idiomatique parmi les 4 propositions suivantes?"
+    choices, gold_idx = get_choices_and_gold_from_line(line, use_context)
     mcq_input = {
         "question": question,
         "choices": choices,
@@ -48,38 +50,69 @@ def process_line(line, use_context, use_instruction):
     return mcq_input
 
 
-# Idiomatic Expressions tasks
-idiomatic_expressions_tasks = [
-    LightevalTaskConfig(
-        name=f"idiomatic_expressions{'_instruct' if use_instruction else ''}{'_context' if use_context else ''}_{formulation.name.lower()}:{subset.lower().replace(' ', '_')}",
-        prompt_function=get_mcq_prompt_function(
-            Language.FRENCH,
-            partial(
-                process_line, use_context=use_context, use_instruction=use_instruction
-            ),
-            formulation=formulation,
-        ),
-        suite=["custom"],
-        hf_repo="OpenLLM-BPI/french_idiomatic_expressions",
-        hf_subset=subset,
-        evaluation_splits=("train",),
-        few_shots_split="train",
-        metrics=get_metrics_for_formulation(
-            formulation,
-            [
-                LogLikelihoodAccMetric(),
-                LogLikelihoodAccMetric(normalization=LogProbTokenNorm()),
-                LogLikelihoodAccMetric(normalization=LogProbCharNorm()),
-            ],
-        ),
-    )
-    for subset in SUBSETS
-    for formulation in all_qa_formulations
-    for use_context in [True, False]
-    for use_instruction in [True, False]
-]
+all_qa_formulations = [MCFFormulation(), CFFormulation(), HybridFormulation()]
 
-TASKS_TABLE = idiomatic_expressions_tasks
+TASKS_TABLE.extend(
+    [
+        LightevalTaskConfig(
+            name=f"idiomatic_expressions_mcq{'_context' if use_context else ''}_{formulation.name.lower()}:{subset.lower().replace(' ', '_')}",
+            prompt_function=get_mcq_prompt_function(
+                Language.FRENCH,
+                partial(
+                    process_line,
+                    use_context=use_context,
+                ),
+                formulation=formulation,
+            ),
+            suite=["custom"],
+            hf_repo="OpenLLM-BPI/french_idiomatic_expressions",
+            hf_subset=subset,
+            evaluation_splits=("train",),
+            few_shots_split="train",
+            metrics=get_metrics_for_formulation(
+                formulation,
+                [
+                    LogLikelihoodAccMetric(),
+                    LogLikelihoodAccMetric(normalization=LogProbTokenNorm()),
+                    LogLikelihoodAccMetric(normalization=LogProbCharNorm()),
+                ],
+            ),
+        )
+        for subset in SUBSETS
+        for formulation in all_qa_formulations
+        for use_context in [True, False]
+    ]
+)
+
+
+#### Idiomatic Expressions Fill in the blank tasks
+def prompt(line, task_name: str = None, use_context: bool = False) -> Doc:
+    choices, gold_idx = get_choices_and_gold_from_line(line, use_context)
+    return Doc(
+        task_name=task_name,
+        query="",
+        choices=choices,
+        gold_index=gold_idx,
+    )
+
+
+TASKS_TABLE.extend(
+    [
+        LightevalTaskConfig(
+            name=f"idiomatic_expressions_fib{'_context' if use_context else ''}:{subset.lower().replace(' ', '_')}",
+            prompt_function=prompt,
+            suite=["custom"],
+            hf_repo="OpenLLM-BPI/french_idiomatic_expressions",
+            hf_subset=subset,
+            evaluation_splits=("train",),
+            few_shots_split="train",
+            metrics=[Metrics.loglikelihood_acc],
+        )
+        for subset in SUBSETS
+        for use_context in [True, False]
+    ]
+)
+
 print(f"Total tasks registered: {len(TASKS_TABLE)}")
 print("Tasks:")
 for task in TASKS_TABLE:
