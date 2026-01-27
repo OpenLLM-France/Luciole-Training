5b1493d7711079d1796c2cb9c59ed18a58b4e10d
Jeronymous <jeromelouradour@gmail.com>
2026-01-27 10:44:17 +0100

    remove useless venv

--- training/evaluation/preload_eval_assets.sh
+++ training/evaluation/preload_eval_assets.sh
@@ -7,7 +7,6 @@ module load arch/h100
 module load anaconda-py3/2024.06
 conda activate eval-env
 
-export OpenLLM_OUTPUT=$qgz_ALL_CCFRSCRATCH/OpenLLM-BPI-output
 export HF_HOME=$qgz_ALL_CCFRSCRATCH/.cache/huggingface
 
 hf download flowaicom/Flow-Judge-v0.1
