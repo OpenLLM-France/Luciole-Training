565bb7e9e3a8c30665a6b4e48caf9abf82cfdaf5
Jeronymous <jeromelouradour@gmail.com>
2026-02-04 12:23:51 +0100

    Add something for tinyBenchmarks.pkl

--- training/evaluation/preload_eval_assets.sh
+++ training/evaluation/preload_eval_assets.sh
@@ -9,7 +9,10 @@ conda activate eval-env
 
 export HF_HOME=$qgz_ALL_CCFRSCRATCH/.cache/huggingface
 
-hf download flowaicom/Flow-Judge-v0.1
-
 python -c "import nltk; nltk.download('punkt_tab'); nltk.download('stopwords'); nltk.download('averaged_perceptron_tagger_eng')" # ifbench
 python -c "from spacy.cli import download; download('en_core_web_sm')"  # ifbench
+
+hf download flowaicom/Flow-Judge-v0.1
+
+# lighteval/src/lighteval/tasks/extended/tiny_benchmarks/tinyBenchmarks.pkl
+lighteval accelerate "pretrained=gpt2" "leaderboard|gsm8k|5" --max-samples 1
