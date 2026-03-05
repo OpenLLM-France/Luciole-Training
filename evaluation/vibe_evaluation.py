50c88c6a22634d2973046e315ea5a28bd487570a
oligou <oliviergouvert@gmail.com>
2026-03-05 16:28:19 +0100

    tune plots

--- evaluation/vibe_evaluation.py
+++ evaluation/vibe_evaluation.py
@@ -3,9 +3,11 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("--model_name", type=str, default="")
+parser.add_argument("model_name", type=str, default="")
 parser.add_argument("--chat", action="store_true")
 parser.add_argument("--save_path", type=str, default="vibe_evaluation_results.txt")
+parser.add_argument("--temperature", type=float, default=0.6)
+parser.add_argument("--top_p", type=float, default=0.9)
 args = parser.parse_args()
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -30,25 +32,31 @@ if __name__ == "__main__":
     with open(args.save_path, "w", encoding="utf-8") as f:
         for i, prompt in enumerate(vibe_prompts):
             if args.chat:
+                # tokenizer.eos_token = "<|im_end|>"
                 input_ids = tokenizer.apply_chat_template(
                     [{"role": "user", "content": prompt}],
                     add_generation_prompt=True,
                     return_tensors="pt",
                 ).to(device)
             else:
-                tokenizer.bos_token_id = tokenizer.eos_token_id
-                input_ids = tokenizer(
-                    prompt, return_tensors="pt", add_special_tokens=True
-                ).input_ids.to(device)
+                # # Option 1
+                # tokenizer.bos_token = "</s>"
+                # tokenizer.bos_token_id = tokenizer.eos_token_id
+                # tokenizer.add_bos_token = True
+                # Option 2
+                # tokenizer.add_bos_token = False
+
+                input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
             with torch.no_grad():
                 output = model.generate(
                     input_ids,
-                    max_length=128,
+                    max_new_tokens=1024,
                     num_return_sequences=1,
                     do_sample=True,
-                    top_k=50,
-                    top_p=0.95,
-                    temperature=1.0,
+                    # top_k=50,
+                    top_p=args.top_p,
+                    temperature=args.temperature,
+                    eos_token_id=[1, 261],
                 )
             generated_text = tokenizer.decode(output[0], skip_special_tokens=False)
             print(f"\n\n#### Example #{i} \n{generated_text}\n")
